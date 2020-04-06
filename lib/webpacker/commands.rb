1af293efae457567cbc9c8992b9dd9f034ac37e5
Vinicius Gama <vinicius.gama@powerhrg.com>
2020-04-06 19:04:05 -0300

    Fix clean task

--- lib/webpacker/commands.rb
+++ lib/webpacker/commands.rb
@@ -5,11 +5,18 @@ class Webpacker::Commands
     @webpacker = webpacker
   end
 
+  # Keeps 2 files, the current file being used is not being considered.
+  # This actually mean we should have at maximum 3 files of each name
   def clean(count = 2)
-    if config.public_output_path.exist? && config.public_manifest_path.exist? && versions.count > count
-      versions.drop(count).flat_map(&:last).each do |file|
-        File.delete(file) if File.file?(file)
-        logger.info "Removed #{file}"
+    if config.public_output_path.exist? && config.public_manifest_path.exist?
+
+      # For each pack drops the 2 more recent files an deletes the rest.
+      # The current file is not in this list, so it will never be delete.
+      packs.each do |pack|
+        pack.files.drop(count).each do |file|
+          File.delete(file)
+          logger.info "Removed #{file}"
+        end
       end
     end
 
@@ -32,12 +39,35 @@ class Webpacker::Commands
   end
 
   private
-    def versions
-      all_files       = Dir.glob("#{config.public_output_path}/**/*")
-      manifest_config = Dir.glob("#{config.public_manifest_path}*")
+    def packs
+      manifest_packs.map do |pack|
+        file_prefix = File.basename(pack, File.extname(pack))
 
-      packs = all_files - manifest_config - current_version
-      packs.group_by { |file| File.mtime(file).utc.to_i }.sort.reverse
+        # For each pack in the manifest
+        # group all files not in the manifest that matches the pack name
+        # ordered by modification time desc
+        file_matches = files_not_in_manifest
+                      .select  { |file_name| File.basename(file_name).starts_with?("#{file_prefix}-") }
+                      .sort_by { |file| File.mtime(file).utc.to_i }.reverse
+
+        OpenStruct.new({ name: pack, files: file_matches })
+      end
+    end
+
+    def manifest_packs
+      manifest.refresh.keys
+    end
+
+    def files_not_in_manifest
+      all_files - manifest_config - current_version
+    end
+
+    def all_files
+      Dir.glob("#{config.public_output_path}/**/*")
+    end
+
+    def manifest_config
+      Dir.glob("#{config.public_manifest_path}*")
     end
 
     def current_version
